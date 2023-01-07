7efd93b77f034af69952c260e2caf84921d9e6fb
Narayan Aryal <aryal.coding@gmail.com>
2023-01-07 13:44:38 +1100

    Api code added

---
+++ src/config/dbConfig.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export const connectMongoDB = () => {
+  try {
+    const mongoUrl = process.env.MONGODB_URL
+    mongoose.set("strictQuery", true); // to suppress the warning in the console
+    const conn = mongoose.connect(mongoUrl);
+    conn && console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error.message, "from ConnectMongoDB function");
+  }
+};
